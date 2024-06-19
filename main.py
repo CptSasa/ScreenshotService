ee3b3da498841e2720d9cbcf23e8dd6585738da4
CptSasa <salim.b@hotmail.de>
2024-06-19 13:26:18 +0200

    functioning prototype. WIP fixing the problem that : and / are automatically converted into %

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from screenshot import Screenhshot
+from screenshot import Screenshot
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 
@@ -16,10 +16,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/image/{url:path}")
+@app.get("/{url:path}")
 def get_image(url: str):
     try:
-        file_path = Screenhshot.take_screenshot(url)
+        file_path = Screenshot.take_screenshot(url)
         return FileResponse(file_path)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
--- screenshot.py
+++ screenshot.py
@@ -1,30 +1,42 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import time
-class Screenhshot:
-    def take_screenshot(url):
+import os
+
+class Screenshot:
+    def take_screenshot(url: str) -> str:
         # Set up Chrome options
         chrome_options = Options()
-    
+        chrome_options.add_argument('--headless')  # Run in headless mode
+        chrome_options.add_argument('--disable-gpu')  # Disable GPU acceleration
+
         # Set up the Chrome driver
         service = ChromeService(ChromeDriverManager().install())
         driver = webdriver.Chrome(service=service, options=chrome_options)
     
         try:
             # Open the URL
-            driver.get(url)
-        
+
+            driver.get('https://' + url)
+            print('https://'+ url)
             # Wait for the page to load
             time.sleep(2)
             driver.maximize_window()
             time.sleep(1)  # Give the window time to maximize
+            
             # Take the screenshot
             screenshot = driver.get_screenshot_as_png()
+            if not os.path.exists("/tmp"):
+                os.makedirs("/tmp")
+            # Save the screenshot to a file
+            file_path = f"/tmp/{url.replace('/', '_')}.png"
+            with open(file_path, "wb") as f:
+                f.write(screenshot)
     
         finally:
             # Close the driver
             driver.quit()
-        return screenshot
+            
+        return file_path
