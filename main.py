f3dc9d90513b772aa26194fb28faaff5e1e4d710
CptSasa <salim.b@hotmail.de>
2024-06-19 12:32:40 +0200

    fixed bug with url

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Union
-from screenshot import take_screenshot
+from screenshot import Screenhshot
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import FileResponse
 
@@ -16,6 +16,10 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 
-@app.get("/image/{image_name}")
-def get_image(image_name: str):
-    return FileResponse(screenshot.take_screenshot(image_name))
+@app.get("/image/{url:path}")
+def get_image(url: str):
+    try:
+        file_path = Screenhshot.take_screenshot(url)
+        return FileResponse(file_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
